b5cdc235f2b08223ae4c96d37e308f56c94f72ca
Luuk van Oijen <>
2021-01-03 02:55:10 +0100

    this hella scuffed function somewhat works?

--- shaders/lighting/rt_conversion.glsl
+++ shaders/lighting/rt_conversion.glsl
@@ -2,8 +2,15 @@
 
 vec2 ATLAS_SIZE = textureSize(TEXTURE_ATLAS, 0).xy;
 
+vec2 roundUV(vec2 uv) {
+	vec2 uvQuad = floor(uv * (2.0 + (1.0 - uv)*uv.y ));
+
+	vec2 t = floor(uv * ATLAS_SIZE / TEXTURE_RESOLUTION + uvQuad);
+	return t * TEXTURE_RESOLUTION / ATLAS_SIZE;
+}
+
 vec2 atlasUVfromBlockUV(vec2 uv, vec2 blockUV) {
 	vec2 uvScale = (vec2(TEXTURE_RESOLUTION * TEXTURE_RESOLUTION) / ATLAS_SIZE) * 0.5;
-	vec2 finalUV = uv + blockUV * uvScale;
+	vec2 finalUV = roundUV(uv) + blockUV * uvScale;
 	return finalUV;
 }
